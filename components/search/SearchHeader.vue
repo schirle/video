d0f515e45fb8ae97219d2f2342b5286d90422665
不想说话 <7@huamail.com>
2024-08-17 15:39:26 +0800

    Update SearchHeader.vue

--- components/search/SearchHeader.vue
+++ components/search/SearchHeader.vue
@@ -31,7 +31,7 @@ console.log(colorMode.preference)
 
         <div class="flex flex-row items-center gap-1">
           <img class="w-[30px] h-[30px] md:w-[40px] md:h-[40px] cursor-pointer" src="@/assets/my-logo.png" alt="logo" @click="goHome()">
-          <h1 class="hidden md:block text-[14px] font-serif font-bold cursor-pointer dark:text-white" @click="goHome()" >爱盼-网盘资源搜索</h1>
+          <h1 class="hidden md:block text-[14px] font-serif font-bold cursor-pointer dark:text-white" @click="goHome()" >搜剧</h1>
         </div>
 
         <div class="w-[220px] md:w-[400px]">
@@ -57,9 +57,7 @@ console.log(colorMode.preference)
               <img class="w-[20px] h-[20px]" src="@/assets/theme/icon-park-solid--dark-mode.svg" alt="">
             </el-button>
           </client-only>
-          <el-button link @click="goGithub()">
-            <img class="w-[20px] h-[20px]" src="@/assets/skill-icons--github-dark.svg" alt="github">
-          </el-button>
+
         </div>
 
       </div>
