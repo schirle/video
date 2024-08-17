07fd1db498b0357c2b71412908a692b6115091eb
不想说话 <7@huamail.com>
2024-08-17 16:06:49 +0800

    Update donate.vue

--- pages/donate.vue
+++ pages/donate.vue
@@ -6,7 +6,7 @@
   <div class="p-[20px]">
     <h1 class=" text-3xl font-bold text-center">打赏</h1>
     <div class="mt-10 flex justify-center">
-      <img style="width: 300px;margin-top: 40px;" src="@/assets/donation/wechat_pay.jpg" alt="打赏">
+      <img style="width: 300px;margin-top: 40px;" src="https://dl.pddpic.com/mms-promotion-file/2024-08-01/6a214c93148424effd1dac3577f314ed.jpg" alt="打赏">
     </div>
   </div>
 </template>
